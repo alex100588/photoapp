93eaf4eea667e0900cdf19364e6570c8c25096ec
alex100588 <alexandru_ionasc@yahoo.com>
2025-01-20 15:22:25 +0200

    first commit

---
+++ components/title.mjs
@@ -0,0 +1,15 @@
+export default class Title{
+    constructor(){
+        this.heading = 'My foto gallery !'
+        this.subHeading = "This is my foto gallery"
+    }
+
+    render() {
+        return `
+          <section class="text-center jumbotron">
+            <h1>${this.heading}</h1>
+            <p>${this.subHeading}</p>
+          </section>
+       `
+      }
+}
