e6f0ecc49dce289fb7a4cfab308f9cc73e522438
alex100588 <alexandru_ionasc@yahoo.com>
2025-01-22 10:41:35 +0200

    grid

--- components/photogrid.mjs
+++ components/photogrid.mjs
@@ -1,20 +1,37 @@
 import Thumbnail from "./thumbnail.mjs";
 
 export default class PhotoGrid {
-  constructor(data){
-    this.data = data
-    console.log(data);
-    
+  constructor(data) {
+    this.data = data;
+    this.thumbnails = [];
   }
+
+  get thumbnailMarkup() {
+    let thumbnailsHTML = "";
+    this.thumbnails.forEach(thumbnail => {
+      thumbnailsHTML += `<div class="col-md-4 col-xs-6">${thumbnail.render()} </div>`
+    })
+   
+    return thumbnailsHTML;
+  }
+
+  //gridud unde se pun toate imaginile
+  componentsWillRender() {
+    if (this.thumbnails.length > 0) {
+      return;
+    }
+    this.data.forEach((thumbnail) =>
+      this.thumbnails.push(new Thumbnail(thumbnail))
+    );
+  }
+
   render() {
+    this.componentsWillRender();
+
     return `
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-xs-6">
-                ${new Thumbnail().render()}
-            </div>
-        </div>
-    </div>
-    `;
+      <div class="container">    
+        ${this.thumbnailMarkup}
+      </div>
+    `
   }
 }
--- components/thumbnail.mjs
+++ components/thumbnail.mjs
@@ -1,9 +1,21 @@
 export default class Thumbnail {
-  constructor() {}
+  constructor(props) {
+    this.props = props;
+  }
+
+  //componenta care trimite o singura imagine pentru container de la care se multiplica celelalte
 
   render() {
     return `
-            <p>Poza</>
-        `;
+        <div class="thumbnail">
+          <img src="${this.props.srcThumbnail}" alt="lamai">
+          <div class="caption">
+            <h3>${this.props.title}</h3>
+            <p>${this.props.shortDesc}</p>
+            <a href="/single-photo?id=${this.props.id}" 
+        " class="btn btn-primary link-button" role="button">See more</a>
+        </div>
+      </div>
+  `
   }
 }
